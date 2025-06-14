f9ca97fb3601ee84818ffbd09e50c30b31c2d35d
Eizeed <eizeed321@gmail.com>
2025-06-14 12:40:40 +0500

    Resolve: #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, NaiveDateTime, Utc};
-use std::{borrow::Cow, str::FromStr, time::Duration};
+use std::{borrow::Cow, time::Duration};
 
 #[derive(Debug, Clone)]
 pub enum Expires {
@@ -54,7 +54,6 @@ impl<'a> TryFrom<&'a str> for Cookie<'a> {
         cookie.name = iter.next().ok_or(ParseError::MissingPair)?.trim().into();
 
         let rest = iter.next().ok_or(ParseError::MissingPair)?.trim();
-
         let mut iter = rest.split(';');
 
         cookie.val = iter.next().unwrap().trim().into();
@@ -105,11 +104,13 @@ fn parse_date(str: &str) -> Result<DateTime<Utc>, ParseError> {
         .ok_or(ParseError::InvalidDate)?
         .trim();
 
-    let date = NaiveDateTime::from_str(date).map_err(|_| ParseError::InvalidDate)?;
+    let date = NaiveDateTime::parse_from_str(date, "%a, %d %b %Y %H:%M:%S")
+        .map_err(|_| ParseError::InvalidDate)?;
 
     Ok(DateTime::from_naive_utc_and_offset(date, Utc))
 }
 
+#[derive(Debug, Clone)]
 pub enum ParseError {
     MissingPair,
     EmptyName,
@@ -117,4 +118,48 @@ pub enum ParseError {
     InvalidSameSite,
     InvalidDate,
     Utf8Error,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn parse_valid() {
+        let date1 = "Tue, 21 Oct 2025 07:28:00 GMT";
+        let date2 = "Mon, 14 Jun 2027 12:00:00 GMT";
+        let date3 = "Thu, 01 Jan 2026 23:59:59 GMT";
+
+        let res1 = parse_date(date1).unwrap();
+        let res2 = parse_date(date2).unwrap();
+        let res3 = parse_date(date3).unwrap();
+
+        assert_eq!(
+            res1,
+            DateTime::<Utc>::from_str("2025-10-21T07:28:00Z").unwrap()
+        );
+        assert_eq!(
+            res2,
+            DateTime::<Utc>::from_str("2027-06-14T12:00:00Z").unwrap()
+        );
+        assert_eq!(
+            res3,
+            DateTime::<Utc>::from_str("2026-01-01T23:59:59Z").unwrap()
+        );
+    }
+
+    #[test]
+    fn parse_invalid() {
+        let invalid_day_of_week = "Wed, 21 Oct 2025 07:28:00 GMT";
+        let invalid_format = "21 Oct 2025 07:28:00";
+        let invalid_tz = "Thu, 01 Jan 2026 23:59:59 UTC";
+        let invalid_time = "Thu, 01 Jan 2026 24:59:59 GMT";
+
+        assert!(parse_date(invalid_day_of_week).is_err());
+        assert!(parse_date(invalid_format).is_err());
+        assert!(parse_date(invalid_tz).is_err());
+        assert!(parse_date(invalid_time).is_err());
+    }
 }
